570e62016a4553961281ac49174bb7277863f064
Iván Amaya <amaya.ivan333@gmail.com>
2026-04-16 04:13:36 -0400

    usar DATABASE_URL de variables de entorno

--- app/database.py
+++ app/database.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:admin@localhost:5432/fintrack"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:TU_CONTRASEÑA@localhost:5432/fintrack")
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
